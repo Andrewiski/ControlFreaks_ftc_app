f576f0bf9ff7067626591e1667cb1ed1d47ff085
Andrewiski <adevries@digitalexample.com>
2017-11-17 14:03:26 -0500

    Pixy Led Works block Read added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlFreaks/PixyCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlFreaks/PixyCamera.java
@@ -3,7 +3,11 @@ package org.firstinspires.ftc.teamcode.ControlFreaks;
 //import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.internal.android.dx.rop.cst.CstArray;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by adevries on 11/13/2017.
@@ -14,13 +18,24 @@ public class PixyCamera {
     private byte I2CAddress = 0x54;
     private Wire v_pixy;
 
+    // Communication/misc parameters
     private static final int PIXY_START_WORD = 0xaa55;
     private static final int PIXY_START_WORD_CC = 0xaa56;
     private static final int PIXY_START_WORDX = 0x55aa;
-
+    private static final int PIXY_MAX_SIGNATURE = 7;
     private static final byte PIXY_SERVO_SYNC = (byte) 0xff;
     private static final byte PIXY_CAM_BRIGHTNESS_SYNC = (byte) 0xfe;
     private static final byte PIXY_LED_SYNC = (byte) 0xfd;
+    // Pixy x-y position values
+    private static final int PIXY_MIN_X  = 0;
+    private static final int PIXY_MAX_X = 319;
+    private static final int PIXY_MIN_Y =  0;
+    private static final int PIXY_MAX_Y = 199;
+
+    // RC-servo values
+    private static final int PIXY_RCS_MIN_POS = 0;
+    private static final int PIXY_RCS_MAX_POS =  1000;
+    private static final int PIXY_RCS_CENTER_POS = ((PIXY_RCS_MAX_POS-PIXY_RCS_MIN_POS)/2);
 
     public enum BlockType {
         NORMAL_BLOCK,
@@ -203,5 +218,161 @@ int setServos(uint16_t s0, uint16_t s1)
         android.util.Log.d("pixyCamera",debugMessage);
         //DbgLog.msg(debugMessage);
         //telemetry.addData(line, debugMessage);
+    }
+    void debugPrint( String msg){
+
+        String debugMessage = logId + msg;
+        android.util.Log.d("pixyCamera",debugMessage);
+        //DbgLog.msg(debugMessage);
+        //telemetry.addData(line, debugMessage);
+    }
+
+    public class Block
+    {
+        // print block structure!
+        String print()
+        {
+            int i, j;
+            String sig = "";
+            int d;
+            Boolean flag;
+            if (signature>PIXY_MAX_SIGNATURE) // color code! (CC)
+            {
+                // convert signature number to an octal string
+                for (i=12, j=0, flag=false; i>=0; i-=3)
+                {
+                    d = (signature>>i)&0x07;
+                    if (d>0 && !flag)
+                        flag = true;
+                    if (flag)
+                        sig = sig +  d + '0';
+                }
+
+                return "CC block! sig: " + signature + " x:" + x + " y:" + y + " width:" + width +  " height:" + height + " angle:" + angle;
+            }
+            else // regular block.  Note, angle is always zero, so no need to print
+                return "sig: " + signature + " x:" + x + " y:" + y + " width:" + width +  "height:" + height;
+
+        }
+        public int signature;
+        public int x;
+        public int y;
+        public int width;
+        public int height;
+        public int angle;
+    };
+
+    private boolean  skipStart = false;
+    private BlockType blockType;
+    private int blockCount = 0;
+
+    private boolean getStart()
+    {
+        int w, lastw;
+
+        lastw = 0xffff;
+
+        while(true)
+        {
+            w = v_pixy.readLH();
+            if (w==0 && lastw==0)
+            {
+                //delayMicroseconds(10);
+                return false;
+            }
+            else if (w==PIXY_START_WORD && lastw==PIXY_START_WORD)
+            {
+                blockType = BlockType.NORMAL_BLOCK;
+                return true;
+            }
+            else if (w==PIXY_START_WORD_CC && lastw==PIXY_START_WORD)
+            {
+                blockType = BlockType.CC_BLOCK;
+                return true;
+            }
+            else if (w==PIXY_START_WORDX)
+            {
+                debugPrint("reorder");
+                v_pixy.read();
+            }
+            lastw = w;
+        }
+    }
+
+    ArrayList<Block> blocks;
+    private int getBlocks(int maxBlocks) {
+        int i;
+        int w, checksum, sum;
+
+        blocks = new ArrayList<Block>();
+        if (!skipStart) {
+            if (getStart() == false)
+                return 0;
+        } else
+            skipStart = false;
+
+        for (blockCount = 0; blockCount < maxBlocks; ) {
+            checksum = v_pixy.readLH();
+            if (checksum == PIXY_START_WORD) // we've reached the beginning of the next frame
+            {
+                skipStart = true;
+                blockType = BlockType.NORMAL_BLOCK;
+                //Serial.println("skip");
+                return blockCount;
+            } else if (checksum == PIXY_START_WORD_CC) {
+                skipStart = true;
+                blockType = BlockType.CC_BLOCK;
+                return blockCount;
+            } else if (checksum == 0) {
+                return blockCount;
+            }
+            Block block = new Block();
+            for (i = 0, sum = 0; i < 6; i++) {
+                if (blockType == BlockType.NORMAL_BLOCK && i >= 5) // skip
+                {
+                    block.angle = 0;
+                    break;
+                }
+                w = v_pixy.readLH();
+                sum += w;
+                switch (i) {
+                    case 0:
+                        block.signature = w;
+                        break;
+                    case 1:
+                        block.x = w;
+                        break;
+                    case 2:
+                        block.y = w;
+                        break;
+                    case 3:
+                        block.width = w;
+                        break;
+                    case 4:
+                        block.height = w;
+                        break;
+                    case 5:
+                        block.angle = w;
+                        break;
+                }
+
+                if (checksum == sum) {
+                    blockCount++;
+                    blocks.add(block);
+                } else {
+                    debugPrint("cs error");
+                }
+
+                w = v_pixy.readLH();
+                if (w == PIXY_START_WORD) {
+                    blockType = BlockType.NORMAL_BLOCK;
+                } else if (w == PIXY_START_WORD_CC) {
+                    blockType = BlockType.CC_BLOCK;
+                } else {
+                    return blockCount;
+                }
+            }
+        }
+        return blockCount;
     }
 }
